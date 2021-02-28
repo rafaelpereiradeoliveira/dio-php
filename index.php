e09fbbff082ad56aa90771046e5ca1a6e2c7b9ff
Rafael Pereira de Oliveira <rafaelpereiradeoliveira@yahoo.com.br>
2021-02-28 09:16:23 -0300

    Commit Inicial

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+    <title>Formulario de Inscricao</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+</head>
+<body>
+    <p>Formulario para inscricao de competidor.</p>
+
+    <form action="script.php" method="post">
+        <p>Your name: <input type="text" name="nome" /></p>
+        <p>Your age: <input type="text" name="idade" /></p>
+        <p><input type="submit" /></p>
+    </form>
+
+</body>
+
+</html>
