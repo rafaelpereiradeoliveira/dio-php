90bc4225f3d095e7e3d182c678843eabebd4f06b
Rafael Pereira de Oliveira <rafaelpereiradeoliveira@yahoo.com.br>
2021-02-28 10:21:43 -0300

    Integração do index com script de qualificação.

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width-device-width, initial-scale=1">
 </head>
 <body>
+
     <p>Formulario para inscricao de competidor.</p>
 
     <form action="script.php" method="post">
